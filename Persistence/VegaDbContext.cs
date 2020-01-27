294af69fcb68ecf42902fb6848ca3b0a0ddfcb96
Akshay Raut <rakshay08@gmail.com>
2020-01-28 05:24:46 +0530

    Working migrations added successfully to database

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -1,9 +1,11 @@
 using Microsoft.EntityFrameworkCore;
+using Vega.Models;
 
 namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
         {
